23f846722cdcd94ced20956d7334e76922cfd445
Barbarasanchez11 <barbarasanchezurbano@gmail.com>
2025-08-04 14:04:59 +0200

    feat: integrate Mapbox map with initial setup and working display

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./components/Map/Map";
+import "./App.css"
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
